943d73863a8328a37f70c2c242e3c7fd9edea39e
fulyagerin <fulyanur.gerin@student.unisg.ch>
2024-04-17 11:02:21 +0200

    hello world print

--- Main.java
+++ Main.java
@@ -8,4 +8,8 @@ public class Main {
     public static void printSomething() {
         System.out.println("Something");
     }
+    public static void printSomethingElse() {
+        System.out.println("Hello World!");
+    }
 }
+
