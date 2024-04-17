48d9b60754d964b5fdc2a42b87753a9b358340eb
MarcelHollenstein <marcel.hollenstein@student.unisg.ch>
2024-04-17 10:58:04 +0200

    feat: add joke

---
+++ Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Why did the Java program go to therapy?");
+        System.out.println("Because it had too many problems with inheritance!");
+    }
+}
