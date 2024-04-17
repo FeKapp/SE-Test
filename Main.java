e48703806fc1fec1570513d8ebe19e8b4a0da4d6
MarcelHollenstein <marcel.hollenstein@student.unisg.ch>
2024-04-17 11:25:14 +0200

    feat: add line

--- Main.java
+++ Main.java
@@ -2,5 +2,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Why did the Java program go to therapy?");
         System.out.println("Because it had too many problems with inheritance!");
+
+        print();
+    }
+
+
+    public static void print() {
+        System.out.println("This is a print statement from the print method.");
     }
 }
