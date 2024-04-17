a2ca856312b809cd1b9bf2f79540522215a37614
Maxinio-berincini <max.beringer@stundent.unisg.ch>
2024-04-17 10:59:43 +0200

    feat: new useful method

--- Main.java
+++ Main.java
@@ -2,5 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Why did the Java program go to therapy?");
         System.out.println("Because it had too many problems with inheritance!");
+        printSomething();
+    }
+
+    public static void printSomething() {
+        System.out.println("Something");
     }
 }
